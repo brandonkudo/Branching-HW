5e9c65f21fe3a3c88f02e51c3392a679bccd53c0
Brandon Kudo <brandonkudo@gmail.com>
2017-09-13 22:29:39 -0700

    add scripts checking age

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  var age = parseInt(prompt("How old are you?"));
 
+  if (age > 21) {
+    $('#drinks').show();
+  } else if (age === 21) {
+    alert("Now don't go crazy!");
+    $('#drinks').show();
+  } else {
+    $('#under-21').show();
+  }
 });
